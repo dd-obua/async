193d7914d4984ea90e91b018820f3a371465586a
Obua Denis Daniel <dd.obua@gmail.com>
2024-08-22 13:44:01 +0300

    Use promises to implement functionalities

--- index.js
+++ index.js
@@ -2,23 +2,39 @@ const fs = require('fs');
 
 const superagent = require('superagent');
 
-const filePath = `${__dirname}/dog.txt`;
-
-fs.readFile(filePath, (error, data) => {
-  if (error) throw new Error(error);
-  console.log(`Breed: ${data}`);
-
-  const breed = data.toString().trim();
-  const url = `https://dog.ceo/api/breed/${breed}/images/random`;
-
-  superagent.get(url).end((err, res) => {
-    if (err) return console.error(err.message);
-
-    const imgUrl = res.body.message;
-    console.log(imgUrl);
-
-    fs.writeFile('dog-img.txt', imgUrl, (error) => {
-      console.log('Random dog image saved to file.');
+const readFilePromise = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, (error, data) => {
+      if (error) reject('Could not find file');
+      resolve(data);
     });
   });
-});
+};
+
+const writeFilePromise = (to, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(to, data, (error) => {
+      if (error) reject('Could not write file');
+      resolve('Success');
+    });
+  });
+};
+
+const filePath = `${__dirname}/dog.txt`;
+
+readFilePromise(filePath)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    const breed = data.toString().trim();
+    const url = `https://dog.ceo/api/breed/${breed}/images/random`;
+    return superagent.get(url);
+  })
+  .then((res) => {
+    const imgUrl = res.body.message;
+    console.log(imgUrl);
+    return writeFilePromise('dog-img.txt', imgUrl);
+  })
+  .then(() => console.log('Random dog image saved to file.'))
+  .catch((error) => {
+    console.error(error);
+  });
