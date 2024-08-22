c8b3c83e37118d15d2b9c8bd9440fef375cf3ec9
Obua Denis Daniel <dd.obua@gmail.com>
2024-08-22 12:08:19 +0300

    Fetch dog breed

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const fs = require('fs');
 
+const superagent = require('superagent');
+
 const filePath = `${__dirname}/dog.txt`;
 
 fs.readFile(filePath, (error, data) => {
   console.log(`Breed: ${data}`);
+
+  const breed = data.toString().trim();
+  const url = `https://dog.ceo/api/breed/${breed}/images/random`;
+
+  superagent.get(url).end((err, res) => {
+    console.log(res.body.message);
+  });
 });
